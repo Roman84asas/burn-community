d217901066f8d377247ddd2cd5f22b06608ef653
Roman84asas <roma.mereneanu@mail.ru>
2020-07-14 18:01:04 +0300

    new pr

---
+++ resources/js/components/Example.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Проверка компонент</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
+}
