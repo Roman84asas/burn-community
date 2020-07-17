4a72c37923bf88ce79ad85a01b1cf3e912015961
Roman84asas <roma.mereneanu@mail.ru>
2020-07-17 14:22:35 +0300

    add roles

---
+++ app/Models/Participant/RoleType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Participant;
+
+use CommerceGuys\Enum\AbstractEnum;
+
+class RoleType extends AbstractEnum
+{
+    public const ADMIN       = 'admin';
+    public const SUPER_ADMIN = 'super_admin';
+    public const MODERATOR   = 'moderator';
+    public const MEMBER_B    = 'member_b';
+    public const MEMBER      = 'member';
+    public const GUEST       = 'guest';
+}
