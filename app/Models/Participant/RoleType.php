25c6d258866581a426bccd7b8ee7d715374312b9
Roman84asas <roma.mereneanu@mail.ru>
2020-07-17 14:29:30 +0300

    add roles

--- app/Models/Participant/RoleType.php
+++ app/Models/Participant/RoleType.php
@@ -8,10 +8,10 @@ use CommerceGuys\Enum\AbstractEnum;
 
 class RoleType extends AbstractEnum
 {
-    public const ADMIN       = 'admin';
-    public const SUPER_ADMIN = 'super_admin';
-    public const MODERATOR   = 'moderator';
-    public const MEMBER_B    = 'member_b';
-    public const MEMBER      = 'member';
-    public const GUEST       = 'guest';
+    public const ADMIN        = 'admin';
+    public const SUPER_ADMIN  = 'super_admin';
+    public const MODERATOR    = 'moderator';
+    public const MEMBER_BTWOB = 'member_btwob';
+    public const MEMBER       = 'member';
+    public const GUEST        = 'guest';
 }
