914e38c6426229e75defd0620025f53f7694eb82
Roman84asas <Sviweruti23>
2020-08-06 21:19:57 +0300

    add tag

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -69,6 +69,7 @@ class LoginController extends Controller
      */
     public function refresh(): Response
     {
+
         return response()->json([
             'success' => true,
             'data'    => [
